1ab9a04089beb9df3fcd0e724094cb89c077af08
DevinFlick <DevinJFlick@gmail.com>
2016-07-21 18:52:47 -0400

    triple check initial workflow, checked syntax and conventions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(cors());
-server.use (express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/public'));
 
 server.get('/', function(req, res){
   res.sendFile('index.html', {root: __dirname + '/public/html'})
